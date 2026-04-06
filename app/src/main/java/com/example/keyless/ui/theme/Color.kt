60958e2fc643e3891316938eaa2efc5d66448ef8
iamnotart <lyndonehemplo1@gmail.com>
2026-04-06 16:44:27 +0800

    App theme setup

--- app/src/main/java/com/example/keyless/ui/theme/Color.kt
+++ app/src/main/java/com/example/keyless/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package com.example.keyless.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val SoftOrange = Color(0xFFFFB068)
+val SoftOrangeContainer = Color(0xFFFFE4C9)
+val DeepOrange = Color(0xFFE48733)
+val WhiteBackground = Color(0xFFFFFFFF)
+val CreamSurface = Color(0xFFFFF7EE)
+val InkText = Color(0xFF3D2A18)
+val AvailableGreen = Color(0xFF44A75A)
+val OccupiedRed = Color(0xFFC84545)
+val DisabledLocker = Color(0xFFE7E1DA)
+val DisabledLockerText = Color(0xFF7D756D)
