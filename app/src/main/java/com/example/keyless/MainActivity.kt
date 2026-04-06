6b58ebf6cf9660351e8c3bc0c43a5605688ac7ae
iamnotart <lyndonehemplo1@gmail.com>
2026-04-06 14:41:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/keyless/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.keyless
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.keyless.ui.theme.KeylessTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KeylessTheme {
+
+            }
+        }
+    }
+}
+
